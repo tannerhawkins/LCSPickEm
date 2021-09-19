588ed25a71bb4c7cff6f4121d73ff13fe875b51f
tannerhawkins <tanner22@live.unc.edu>
2021-09-19 19:47:23 -0400

    Updates

---
+++ static/js/3.02deb6bc.chunk.js
@@ -0,0 +1,163 @@
+(this.webpackJsonpAncientPathAdventures=this.webpackJsonpAncientPathAdventures||[]).push([[3],{82:function(e,t,i){},90:function(e,t,i){"use strict";i.r(t);i(2);var n=i(47),a=(i(82),i(22)),s=i(15),o=i.p+"static/media/homepage-splash.82706964.png",c=i(33),d=i(4);const r=s.a.div`
+    display: flex;
+    flex-flow: column wrap;
+    height: 775px;
+    background-color: ${a.a.COLOR.GREEN};
+    position: relative;
+    display: flex;
+`,l=s.a.img`
+    position: absolute;
+    right: 0;
+    height: 100%;
+    width: 60%;
+    max-width: 600px;
+`,x=s.a.h1`
+    color: white;
+    font-weight: 700;
+    font-size: 73px;
+    line-height: 86px;
+`,p=s.a.h3`
+    color: ${a.a.COLOR.LIGHT_GREEN};
+    font-weight: 300;
+    font-size: 37px;
+    line-height: 43px;
+`,h=s.a.div`
+    margin-left: 10%;
+    margin-top: 8%;
+    display: flex;
+    flex-flow: column wrap;
+    max-width: 35%;
+    z-index: 1;
+`,m=s.a.div`
+    display: flex;
+    margin-top: 5%;
+    margin-left: 10%;
+    z-index: 1;
+`,j=Object(s.a)(c.a)`
+    font-size: 30px;
+    line-height: 36px;
+    font-weight: 700;
+    max-width: 200px;
+    height: 60px;
+`,g=Object(s.a)(c.a)`
+    background-color: ${a.a.COLOR.DARK_GREEN};
+    margin-left: 19px;
+    font-size: 30px;
+    line-height: 36px;
+    font-weight: 700;
+    max-width: 200px;
+    height: 60px;
+`;var b=()=>Object(d.jsxs)(r,{children:[Object(d.jsxs)(h,{children:[Object(d.jsx)(x,{children:"SOME CATCHY TEXT"}),Object(d.jsx)(p,{children:"Some convincing text should go here"})]}),Object(d.jsxs)(m,{children:[Object(d.jsx)(j,{children:"BTN"}),Object(d.jsx)(g,{children:"BTN2"})]}),Object(d.jsx)(l,{src:o})]}),u=i.p+"static/media/homepage-splash-2.df6b84b2.png",f=i.p+"static/media/circle.bc41d2fd.png",O=i.p+"static/media/square.94282324.png";const w=s.a.div`
+    position: relative;
+`,v=s.a.div`
+    height: 55vw;
+    width: 100%;
+    @media (max-width: 1000px) {
+        display: flex;
+        flex-flow: column;
+        height: auto;
+    }
+`,y=s.a.div`
+    background: url(${u}) no-repeat; 
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: 100% 100%;
+`,z=s.a.div`
+    display: flex;
+    justify-content: center;
+    flex-flow: row wrap;
+`,E=s.a.div`
+    display: flex;
+    min-width: 500px;
+    margin-left: 30px;
+    align-items: center;
+    justify-content: space-between;
+    @media (max-width: 1000px) {
+        min-width: 0px;
+        justify-content: space-around;
+        margin: 0 auto;
+        width: 90%;
+    }
+`,R=s.a.div`
+    @media (max-width: 1000px) {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding-top: 30px;
+    }
+`,L=s.a.img`
+    position: absolute;
+    width: 20%;
+    max-width: 350px;
+    @media (max-width: 1000px) {
+        position: relative;
+        width: 40%;
+        margin: 0 auto !important;
+    }
+`,k=s.a.img`
+    width: 60%;
+    max-width: 400px;
+    @media (max-width: 1000px) {
+        width: 40%;
+        min-width: 0px;
+    }
+`,A=s.a.p`
+    font-size: 40px;
+    width: 300px;
+    img+&{
+        margin-left: 50px;
+    }
+    @media (max-width: 600px) {
+        font-size: 4vw;
+        img+&{
+            margin-left: 20px;
+        }
+    }
+`,P=s.a.p`
+    position: absolute;
+    font-weight: 300;
+    font-size: min(2.3vw, 40px);
+    margin: 0 auto;
+    margin-left: 40px;
+    width: 20%;
+    max-width: 300px;
+    font-weight: 500;
+    @media (max-width: 1000px) {
+        position: relative;
+        width: 40%;
+        font-size: 4vw;
+        margin: 0 auto !important;
+    }
+`,T=s.a.h1`
+    color: ${a.a.COLOR.DARK_GREEN};
+    font-size: 90px;
+    padding-left: 12%;
+    padding-top: 300px;
+    @media (max-width: 1000px) {
+        font-size: 50px;
+        padding-top: 30vw;
+        text-align: center;
+        padding-left: 0%;
+    }
+`;var C=()=>Object(d.jsxs)(w,{children:[Object(d.jsxs)(v,{children:[Object(d.jsxs)(R,{children:[Object(d.jsx)(L,{src:f,style:{margin:"5% 0% 0% 5%"}}),Object(d.jsx)(P,{style:{margin:"5% 0% 0% 28%"},children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."})]}),Object(d.jsxs)(R,{children:[Object(d.jsx)(L,{src:f,style:{margin:"10% 0% 0% 50%"}}),Object(d.jsx)(P,{style:{margin:"10% 0% 0% 75%"},children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."})]}),Object(d.jsxs)(R,{children:[Object(d.jsx)(P,{style:{margin:"30% 0% 0% 5%"},children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}),Object(d.jsx)(L,{src:f,style:{margin:"35% 0% 0% 30%"}})]}),Object(d.jsx)(R,{children:Object(d.jsx)(P,{style:{margin:"35% 0% 0% 60%"},children:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."})})]}),Object(d.jsxs)(y,{children:[Object(d.jsx)(T,{children:"HEADING TEXT"}),Object(d.jsxs)(z,{children:[Object(d.jsxs)(E,{children:[Object(d.jsx)(k,{src:O}),Object(d.jsx)(A,{children:"Put some type of text here"})]}),Object(d.jsxs)(E,{children:[Object(d.jsx)(k,{src:O}),Object(d.jsx)(A,{children:"Put some type of text here"})]}),Object(d.jsxs)(E,{children:[Object(d.jsx)(A,{children:"Put some type of text here"}),Object(d.jsx)(k,{src:O})]}),Object(d.jsxs)(E,{children:[Object(d.jsx)(A,{children:"Put some type of text here"}),Object(d.jsx)(k,{src:O})]})]})]})]}),G=i(61);const N=s.a.div`
+    display: flex;
+    background-color: ${a.a.COLOR.DARK_GREEN};
+    justify-content: center;
+    align-items: center;
+    flex-flow: column;
+    height: 80px;
+	position: relative;
+	width: 100%;
+    z-index: 100000;
+    overflow: hidden;
+`,$=s.a.p`
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 43px;
+    color: white;
+`,q=s.a.p`
+    font-size: 15px;
+    color: ${a.a.COLOR.LIGHT_GREEN};
+`;var D=()=>Object(d.jsxs)(N,{id:"footer",children:[Object(d.jsx)($,{children:"Terms of Use  -  Privacy Policy"}),Object(d.jsx)(q,{children:"Copyright \xa9 2021 Ancient Path Adventures"})]});t.default=()=>Object(d.jsxs)(n.a,{description:"Project Description Homepage",children:[Object(d.jsx)(G.a,{}),Object(d.jsx)(b,{}),Object(d.jsx)(C,{}),Object(d.jsx)(D,{})]})}}]);
+//# sourceMappingURL=3.02deb6bc.chunk.js.map
