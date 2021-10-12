badfe7b69d10fc58898df647185a2925cf98ad81
tannerhawkins <tannerhawkins2703@gmail.com>
2021-10-12 12:41:51 -0400

    Started Module work

--- src/app/module/reducers.js
+++ src/app/module/reducers.js
@@ -1,36 +1,24 @@
 const initialState = {
-  signedIn: false,
+  currentStep: null,
+  inModule: false,
 };
 
 // Use the initialState as a default value
-export default function accountReducer(state = initialState, action) {
+export default function moduleReducer(state = initialState, action) {
   // The reducer normally looks at the action type field to decide what happens
   switch (action.type) {
     // Do something here based on the different types of actions
-    case "SIGN_IN":
+    case "SET_CURRENT_STEP":
       return {
         ...state,
-        ...action.payload,
-        signedIn: true,
-        user: action.payload,
-      };
-
-    case "SIGN_OUT":
-      return {
-        initialState,
-      };
-
-    case "UPDATE_CLASS_LIST":
-      return {
-        ...state,
-        classList: action.payload,
+        currentStep: action.payload,
       };
 
     case "CLEAR_STATE":
       return {
-        initialState
+        initialState,
       };
-
+      
     default:
       // If this reducer doesn't recognize the action type, or doesn't
       // care about this specific action, return the existing state unchanged
---
+++ src/pages/Student/Module/ModuleSummary.js
@@ -0,0 +1,8 @@
+
+
+const ModuleSummary = () => {
+
+  return <div>This is the Module Summary Page</div>;
+};
+
+export default ModuleSummary;
