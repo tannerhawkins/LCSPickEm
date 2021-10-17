82a94fb32c72419121039cda5cb008d70e360e30
tannerhawkins <tanner22@live.unc.edu>
2021-10-17 15:09:01 -0400

    fixed routing issue

--- src/components/Authentication/SignInOutButton.js
+++ src/components/Authentication/SignInOutButton.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { auth } from "../../data/firebase.js";
 import { useDispatch, useSelector } from "react-redux";
-import { signOut } from "../../app/account/actions.js";
 import StyledButton from "../Template/Button.js";
 import { Constants } from "../../data/constants.js";
 import styled from "styled-components";
 import { selectIsSignedIn } from "../../app/account/selectors.js";
 import { clearState } from "../../app/store.js";
+import { useHistory } from "react-router";
 
 const SignInOutButton = ({ className }) => {
+  const history = useHistory();
   const dispatch = useDispatch();
   const loginState = useSelector(selectIsSignedIn);
 
@@ -22,9 +23,7 @@ const SignInOutButton = ({ className }) => {
     <div className={className}>
       {!loginState && (
         <StyledSignInButton
-          onClick={() =>
-            (window.location.href = "/AncientPathAdventures/signin")
-          }
+          onClick={() => history.push('/signin')}
         >
           SIGN-IN
         </StyledSignInButton>
