27839d6a7966635313559de2399dbe439e55b4df
alx00ho <alx00ho@gmail.com>
2021-10-17 13:35:35 -0400

    Update ModuleSummary.js

--- src/pages/Student/Module/ModuleSummary.js
+++ src/pages/Student/Module/ModuleSummary.js
@@ -5,6 +5,8 @@ import {
 } from "../../../app/module/selectors";
 import { setCurrentStep } from "../../../app/module/actions";
 import { useHistory } from "react-router";
+import { Constants } from "../../../data/constants";
+import Button from "../../../components/Template/Button";
 
 const ModuleSummary = () => {
   const dispatch = useDispatch();
@@ -13,19 +15,61 @@ const ModuleSummary = () => {
   const module = useSelector(selectSelectedModule)
 
   return <Body>
-    <button onClick={() => history.push("/student/home")}>Back to Home</button>
-    This is the Module Summary Page:
-    {module.title}
-    {module.description}
+    <StyledButtonContainer>
+      <StyledSubmitButton onClick={() => history.push("/student/home")}>
+        Back to Home
+      </StyledSubmitButton>
+    </StyledButtonContainer>
+    
+    <StyledTitle>This is the {module.title} Summary Page:</StyledTitle>
+    <StyledBody>{module.description}</StyledBody>
 
     The summary of this page is simple and short that you can read it very quick for the purpose of this assignment called the "Walking Skeleton".
-    <button onClick={() => dispatch(setCurrentStep(currentStep + 1))}>Next Step</button>
+    
+    <StyledButtonContainer>
+      <StyledSubmitButton onClick={() => dispatch(setCurrentStep(currentStep + 1))}>
+        Next Step
+      </StyledSubmitButton>
+    </StyledButtonContainer>
     </Body>;
 };
 
 const Body = styled.div`
   width: 100%;
   height: 100%;
+`;
+
+const StyledTitle = styled.p`
+  color: ${Constants.COLOR.GREEN};
+  line-height: 36px;
+  font-size: 30px;
+  padding-left: 30px;
+`;
+
+const StyledBody = styled.div`
+  margin-top: ${Constants.HEADER_HEIGHT};
+  margin-left: ${Constants.SIDEBAR_WIDTH};
+  padding: 0 50px;
+`;
+
+const StyledButtonContainer = styled.div`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 30px;
+  margin-left: ${Constants.SIDEBAR_WIDTH};
+`;
+
+const StyledSubmitButton = styled(Button)`
+  font-size: 25px;
+  height: 70px;
+  padding: 0 5px;
+  min-width: 120px;
+  background-color: ${Constants.COLOR.DARK_GREEN};
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 export default ModuleSummary;
