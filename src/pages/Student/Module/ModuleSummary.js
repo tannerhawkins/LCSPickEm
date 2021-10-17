bacd86bab4d89c92c328a5c325332cc8c426032d
alx00ho <alx00ho@gmail.com>
2021-10-17 13:05:51 -0400

    Update ModuleSummary.js

--- src/pages/Student/Module/ModuleSummary.js
+++ src/pages/Student/Module/ModuleSummary.js
@@ -1,16 +1,24 @@
 import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import {
-  selectCurrentStep,
+  selectCurrentStep, selectSelectedModule,
 } from "../../../app/module/selectors";
 import { setCurrentStep } from "../../../app/module/actions";
+import { useHistory } from "react-router";
 
 const ModuleSummary = () => {
   const dispatch = useDispatch();
   const currentStep = useSelector(selectCurrentStep);
+  const history = useHistory();
+  const module = useSelector(selectSelectedModule)
 
   return <Body>
-    This is the Module Summary Page
+    <button onClick={() => history.push("/student/home")}>Back to Home</button>
+    This is the Module Summary Page:
+    {module.title}
+    {module.description}
+
+    The summary of this page is simple and short that you can read it very quick for the purpose of this assignment called the "Walking Skeleton".
     <button onClick={() => dispatch(setCurrentStep(currentStep + 1))}>Next Step</button>
     </Body>;
 };
