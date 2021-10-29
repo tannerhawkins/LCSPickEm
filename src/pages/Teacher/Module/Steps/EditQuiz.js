23984e41e7ae176f40e05494b020d80d6249cf15
tannerhawkins <tannerhawkins2703@gmail.com>
2021-10-29 13:02:41 -0400

    began work on edit quiz

--- src/pages/Teacher/Module/Steps/EditQuiz.js
+++ src/pages/Teacher/Module/Steps/EditQuiz.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Main from "../../../../layouts/Main";
 import SideBar from "../../../../components/Template/SideBar";
 import Header from "../../../../components/Teacher/Header";
 import styled from "styled-components";
 import { Constants } from "../../../../data/constants";
+import Button from "../../../../components/Template/Button";
 
 const EditQuiz = () => {
+  const [questions, setQuestions] = useState([]);
+
+
   return (
     <Main title={"Edit Quiz Step"} description={"Edit Quiz Step"}>
       <SideBar />
       <Header />
       <StyledBody>
         <StyledSectionTitle>Edit Quiz</StyledSectionTitle>
+        <StyledAddQuestionButton>Add Question</StyledAddQuestionButton>
+        {questions.length != 0 && <StyledSubmitButton>Submit</StyledSubmitButton>}
       </StyledBody>
     </Main>
   );
@@ -22,6 +28,23 @@ const StyledBody = styled.div`
   margin-left: ${Constants.SIDEBAR_WIDTH};
   padding: 0 50px;
 `;
+
+const StyledAddQuestionButton = styled(Button)`
+height: 60px;
+width: 130px;
+margin-top: 40px;
+background-color: ${Constants.COLOR.GREEN};
+border-radius: 40px;
+position: absolute;
+right: 50px;
+`
+
+const StyledSubmitButton = styled(Button)`
+  height: 60px;
+  width: 130px;
+  margin-top: 40px;
+  background-color: ${Constants.COLOR.DARK_GREEN};
+`
 
 const StyledSectionTitle = styled.p`
   font-size: 36px;
