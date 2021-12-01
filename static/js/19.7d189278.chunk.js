9e8272d06a0809d0381251408b39f34ea2ad1ac1
tannerhawkins <tannerhawkins2703@gmail.com>
2021-12-01 17:36:05 -0500

    Updates

--- static/js/19.7d189278.chunk.js
+++ static/js/19.7d189278.chunk.js
@@ -1,4 +1,4 @@
-(this.webpackJsonpAncientPathAdventures=this.webpackJsonpAncientPathAdventures||[]).push([[19],{108:function(e,t,n){"use strict";t.a=n.p+"static/media/homepage-splash.82706964.png"},123:function(e,t,n){"use strict";n.r(t),n.d(t,"Book1",(function(){return i})),n.d(t,"Book2",(function(){return a})),n.d(t,"Book3",(function(){return o})),n.d(t,"Book4",(function(){return c}));var i=n.p+"static/media/Book1_In_the_Beginning_ebook.0910fa27.epub",a=n.p+"static/media/Book2_Man_and_Womand_ebook.6be3a873.epub",o=n.p+"static/media/Book3_The_First_Sin.27b501dc.epub",c=n.p+"static/media/Book4_Cain_and_Abel_ebook.61855556.epub"},244:function(e,t,n){"use strict";n.r(t);var i=n(16),a=n(66),o=n(69),c=n(36),r=n(68),s=n(11),d=n(25),l=n(67),p=(n(128),n(161)),x=n(5);var u=e=>{const t=Object(i.c)(o.c);return Object(x.jsx)(h,{children:Object(x.jsx)(b,{...e,percent:t,filledBackground:`linear-gradient(to right, ${d.a.COLOR.TEAL}, ${d.a.COLOR.GREEN})`,width:"90%"})})};const b=Object(a.a)(p.ProgressBar)`
+(this.webpackJsonpAncientPathAdventures=this.webpackJsonpAncientPathAdventures||[]).push([[19],{108:function(e,t,n){"use strict";t.a=n.p+"static/media/homepage-splash.82706964.png"},123:function(e,t,n){"use strict";n.r(t),n.d(t,"Book1",(function(){return i})),n.d(t,"Book2",(function(){return a})),n.d(t,"Book3",(function(){return o})),n.d(t,"Book4",(function(){return c}));var i=n.p+"static/media/Book1_In_the_Beginning_ebook.0910fa27.epub",a=n.p+"static/media/Book2_Man_and_Womand_ebook.6be3a873.epub",o=n.p+"static/media/Book3_The_First_Sin.27b501dc.epub",c=n.p+"static/media/Book4_Cain_and_Abel_ebook.61855556.epub"},244:function(e,t,n){"use strict";n.r(t);var i=n(16),a=n(66),o=n(69),c=n(36),r=n(68),s=n(11),d=n(25),l=n(67),p=(n(128),n(161)),b=n(5);var u=e=>{const t=Object(i.c)(o.c);return Object(b.jsx)(h,{children:Object(b.jsx)(x,{...e,percent:t,filledBackground:`linear-gradient(to right, ${d.a.COLOR.TEAL}, ${d.a.COLOR.GREEN})`,width:"90%"})})};const x=Object(a.a)(p.ProgressBar)`
   box-shadow: 0 4% 4% rgba(0, 0, 0, 0.25);
   color: white;
   background-color: rgba(211, 211, 211, 0.8);
@@ -62,13 +62,14 @@
   justify-content: flex-end;
   margin: 3%;
   margin-top: 3%;
-`;var E=()=>{const e=Object(i.b)(),t=Object(i.c)(o.a),n=Object(s.g)(),a=Object(i.c)(o.f);return Object(x.jsxs)(O,{children:[Object(x.jsx)(m,{children:Object(x.jsx)(v,{"data-test":"home",onClick:()=>n.push("/student/home"),children:"Back to Home"})}),Object(x.jsxs)(g,{children:[a.title," Summary:"]}),Object(x.jsxs)(f,{children:[a.description,Object(x.jsx)(y,{children:"Note: You CANNOT back out of a module once you begin!"})]}),Object(x.jsx)(u,{}),Object(x.jsx)(w,{children:Object(x.jsx)(v,{"data-test":"start",onClick:()=>e(Object(r.b)(t+1)),children:"Start Module"})})]})};const R=a.a.div`
+`;var E=()=>{const e=Object(i.b)(),t=Object(i.c)(o.a),n=Object(s.g)(),a=Object(i.c)(o.f);return Object(b.jsxs)(O,{children:[Object(b.jsx)(m,{children:Object(b.jsx)(v,{"data-test":"home",onClick:()=>n.push("/student/home"),children:"Back to Home"})}),Object(b.jsxs)(g,{children:[a.title," Summary:"]}),Object(b.jsxs)(f,{children:[a.description,Object(b.jsx)(y,{children:"Note: You CANNOT back out of a module once you begin!"})]}),Object(b.jsx)(u,{}),Object(b.jsx)(w,{children:Object(b.jsx)(v,{"data-test":"start",onClick:()=>e(Object(r.b)(t+1)),children:"Start Module"})})]})};const R=a.a.div`
   display: flex;
   flex-direction: column;
   width: 100%;
   min-height: 100vh;
   background: url(${j.a}) no-repeat;
-  background-position: 0 200px;
+  background-size: 100% 90%;
+  background-position: center bottom;
 `,k=Object(a.a)(u)`
   
 `,_=a.a.p`
@@ -97,7 +98,7 @@
   &:hover {
     cursor: pointer;
   }
-`;var L=()=>{const e=Object(i.b)(),t=Object(i.c)(o.b),n=Object(i.c)(o.a),a=Object(i.c)(o.f);return Object(x.jsxs)(R,{children:[Object(x.jsx)(_,{children:a.title}),Object(x.jsx)(C,{dangerouslySetInnerHTML:{__html:t}}),Object(x.jsx)(k,{}),Object(x.jsx)(S,{onClick:()=>e(Object(r.b)(n+1)),"data-test":"next-step",children:"Next Step"})]})},T=n(2),N=n(92);const $=a.a.div`
+`;var L=()=>{const e=Object(i.b)(),t=Object(i.c)(o.b),n=Object(i.c)(o.a),a=Object(i.c)(o.f);return Object(b.jsxs)(R,{children:[Object(b.jsx)(_,{children:a.title}),Object(b.jsx)(C,{dangerouslySetInnerHTML:{__html:t}}),Object(b.jsx)(k,{}),Object(b.jsx)(S,{onClick:()=>e(Object(r.b)(n+1)),"data-test":"next-step",children:"Next Step"})]})},T=n(2),N=n(92);const $=a.a.div`
   background-color: ${d.a.COLOR.DARK_GREEN};
   width: 90%;
   min-height: 200px;
@@ -153,13 +154,13 @@
   &:hover {
     cursor: pointer;
   }
-`);var z=()=>{const e=Object(i.b)(),t=Object(i.c)(o.a),n=Object(i.c)(o.b).questions,[a,c]=Object(T.useState)(0),[s,d]=Object(T.useState)(!1),[l,p]=Object(T.useState)(0);return Object(x.jsx)($,{children:s?Object(x.jsxs)(A,{"data-test":"score",children:["You scored ",l," out of ",n.length,Object(x.jsx)(G,{onClick:()=>e(Object(r.b)(t+1)),"data-test":"next-step",children:"Submit Quiz"})]}):Object(x.jsx)(N.a,{currentQuestion:a,questions:n,handleAnswerOptionClick:e=>{e&&p(l+1);const t=a+1;t<n.length?c(t):d(!0)}})})};const D=a.a.div`
+`);var z=()=>{const e=Object(i.b)(),t=Object(i.c)(o.a),n=Object(i.c)(o.b).questions,[a,c]=Object(T.useState)(0),[s,d]=Object(T.useState)(!1),[l,p]=Object(T.useState)(0);return Object(b.jsx)($,{children:s?Object(b.jsxs)(A,{"data-test":"score",children:["You scored ",l," out of ",n.length,Object(b.jsx)(G,{onClick:()=>e(Object(r.b)(t+1)),"data-test":"next-step",children:"Submit Quiz"})]}):Object(b.jsx)(N.a,{currentQuestion:a,questions:n,handleAnswerOptionClick:e=>{e&&p(l+1);const t=a+1;t<n.length?c(t):d(!0)}})})};const D=a.a.div`
   width: 100vw;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
-`;var B=()=>{Object(i.b)(),Object(i.c)(o.a);return Object(x.jsx)(D,{children:Object(x.jsx)(z,{})})},P=n(108);const H=a.a.div`
+`;var B=()=>{Object(i.b)(),Object(i.c)(o.a);return Object(b.jsx)(D,{children:Object(b.jsx)(z,{})})},P=n(108);const H=a.a.div`
   display: flex;
   justify-content: flex-center;
   flex-direction: column;
@@ -192,7 +193,7 @@
   &:hover {
     cursor: pointer;
   }
-`;var M=()=>{const e=Object(i.b)(),t=Object(i.c)(o.a),n=Object(i.c)(o.b),a=Object(i.c)(o.f);return Object(x.jsxs)(H,{children:[Object(x.jsxs)(q,{children:["This is the ",a.title," Video Page"]}),Object(x.jsx)(I,{frameborder:"0",src:`${n}`,allow:"autoplay;",sandbox:"allow-scripts allow-same-origin",allowFullScreen:!0}),Object(x.jsx)(u,{}),Object(x.jsx)(K,{children:Object(x.jsx)(Q,{onClick:()=>e(Object(r.b)(t+1)),"data-test":"next-step",children:"Next Step"})})]})},F=n(133),U=n(123);const W=a.a.div`
+`;var M=()=>{const e=Object(i.b)(),t=Object(i.c)(o.a),n=Object(i.c)(o.b),a=Object(i.c)(o.f);return Object(b.jsxs)(H,{children:[Object(b.jsx)(q,{children:a.title}),Object(b.jsx)(I,{frameborder:"0",src:`${n}`,allow:"autoplay;",sandbox:"allow-scripts allow-same-origin",allowFullScreen:!0}),Object(b.jsx)(u,{}),Object(b.jsx)(K,{children:Object(b.jsx)(Q,{onClick:()=>e(Object(r.b)(t+1)),"data-test":"next-step",children:"Next Step"})})]})},F=n(133),U=n(123);const W=a.a.div`
   display: flex;
   justify-content: flex-center;
   flex-direction: column;
@@ -230,10 +231,10 @@
   &:hover {
     cursor: pointer;
   }
-`;var Z=()=>{const e=Object(i.b)(),t=Object(i.c)(o.a),n=Object(i.c)(o.h),a=Object(s.g)(),[c,l]=(Object(i.c)(o.f),Object(T.useState)([])),p=Object(T.useRef)(null),[u,b]=Object(T.useState)(),[h,j]=Object(T.useState)(null),[O,g]=Object(T.useState)(!1),[f,m]=Object(T.useState)(""),v={...F.ReactReaderStyle,container:{...F.ReactReaderStyle.container,height:"auto"},arrow:{...F.ReactReaderStyle.arrow,color:d.a.COLOR.GREEN},tocArea:{display:"none"},tocButton:{display:"none"},readerArea:{...F.ReactReaderStyle.readerArea,width:"120vh",maxWidth:"100%",maxHeight:"700px",height:"50vw"}};return Object(x.jsxs)(W,{children:[Object(x.jsx)(J,{children:f}),Object(x.jsx)(Y,{children:Object(x.jsx)(F.ReactReader,{location:u,locationChanged:e=>{b(e),p.current.location.end.index===h&&g(!0)},url:U[n[t].data],styles:v,getRendition:e=>{p.current=e,p.current.themes.default({"::selection":{background:"yellow"}}),e.book.locations.generate(),j(e.book.pageList.lastPage-1),m(e.book.packaging.metadata.title),p.current=e,l([])},epubOptions:{allowScriptedContent:!0}})}),Object(x.jsxs)(V,{children:[Object(x.jsx)(X,{"data-test":"home",onClick:()=>a.push("/student/home"),children:"Back to Home"}),Object(x.jsx)(X,{"data-test":"start",onClick:()=>e(Object(r.b)(t+1)),style:{display:O?"flex":"none"},children:"Next Step"})]})]})};const ee=a.a.div`
+`;var Z=()=>{const e=Object(i.b)(),t=Object(i.c)(o.a),n=Object(i.c)(o.h),a=Object(s.g)(),[c,l]=(Object(i.c)(o.f),Object(T.useState)([])),p=Object(T.useRef)(null),[u,x]=Object(T.useState)(),[h,j]=Object(T.useState)(null),[O,g]=Object(T.useState)(!1),[f,m]=Object(T.useState)(""),v={...F.ReactReaderStyle,container:{...F.ReactReaderStyle.container,height:"auto"},arrow:{...F.ReactReaderStyle.arrow,color:d.a.COLOR.GREEN},tocArea:{display:"none"},tocButton:{display:"none"},readerArea:{...F.ReactReaderStyle.readerArea,width:"120vh",maxWidth:"100%",maxHeight:"700px",height:"50vw"}};return Object(b.jsxs)(W,{children:[Object(b.jsx)(J,{children:f}),Object(b.jsx)(Y,{children:Object(b.jsx)(F.ReactReader,{location:u,locationChanged:e=>{x(e),p.current.location.end.index===h&&g(!0)},url:U[n[t].data],styles:v,getRendition:e=>{p.current=e,p.current.themes.default({"::selection":{background:"yellow"}}),e.book.locations.generate(),j(e.book.pageList.lastPage-1),m(e.book.packaging.metadata.title),p.current=e,l([])},epubOptions:{allowScriptedContent:!0}})}),Object(b.jsxs)(V,{children:[Object(b.jsx)(X,{"data-test":"home",onClick:()=>a.push("/student/home"),children:"Back to Home"}),Object(b.jsx)(X,{"data-test":"start",onClick:()=>e(Object(r.b)(t+1)),style:{display:O?"flex":"none"},children:"Next Step"})]})]})};const ee=a.a.div`
   width: 100%;
   height: 100%;
-`;t.default=()=>{var e;const t=Object(s.g)(),n=Object(i.c)(o.f),a=Object(i.c)(o.a),r=-1===a||null===n||void 0===n?void 0:n.steps[a];return Object(x.jsx)(c.a,{title:null===n||void 0===n?void 0:n.title,description:"Module Page",children:Object(x.jsxs)(ee,{children:[-1===a&&Object(x.jsx)(E,{}),r&&(()=>{switch(r.type){case"text":return Object(x.jsx)(L,{});case"video":return Object(x.jsx)(M,{});case"quiz":return Object(x.jsx)(B,{});case"ebook":return Object(x.jsx)(Z,{});default:return void t.push("/student/home")}})(),a>=(null===n||void 0===n||null===(e=n.steps)||void 0===e?void 0:e.length)&&t.push("/student/home")]})})}},67:function(e,t,n){"use strict";var i=n(25);const a=n(66).a.div`
+`;t.default=()=>{var e;const t=Object(s.g)(),n=Object(i.c)(o.f),a=Object(i.c)(o.a),r=-1===a||null===n||void 0===n?void 0:n.steps[a];return Object(b.jsx)(c.a,{title:null===n||void 0===n?void 0:n.title,description:"Module Page",children:Object(b.jsxs)(ee,{children:[-1===a&&Object(b.jsx)(E,{}),r&&(()=>{switch(r.type){case"text":return Object(b.jsx)(L,{});case"video":return Object(b.jsx)(M,{});case"quiz":return Object(b.jsx)(B,{});case"ebook":return Object(b.jsx)(Z,{});default:return void t.push("/student/home")}})(),a>=(null===n||void 0===n||null===(e=n.steps)||void 0===e?void 0:e.length)&&t.push("/student/home")]})})}},67:function(e,t,n){"use strict";var i=n(25);const a=n(66).a.div`
   background-color: ${i.a.COLOR.LIGHT_GREEN};
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   display: flex;
@@ -276,4 +277,4 @@
   cursor: pointer;
   margin: 10px 0px;
 `;t.a=e=>Object(o.jsxs)(o.Fragment,{children:[Object(o.jsxs)(c,{children:[Object(o.jsxs)(r,{children:[Object(o.jsxs)("span",{children:["Question ",e.currentQuestion+1]}),"/",e.questions.length]}),Object(o.jsx)(s,{"data-test":"question-text",children:e.questions[e.currentQuestion].questionText})]}),Object(o.jsx)(d,{children:e.questions[e.currentQuestion].answerOptions.map((t=>Object(o.jsx)(l,{onClick:()=>e.inEdit?void 0:e.handleAnswerOptionClick(t.isCorrect),style:{height:100/e.questions[e.currentQuestion].answerOptions.length+"%"},"data-test":"answer-text",children:t.answerText},e.questions[e.currentQuestion].answerOptions.indexOf(t))))})]})}}]);
-//# sourceMappingURL=19.86ad9dbb.chunk.js.map
+//# sourceMappingURL=19.7d189278.chunk.js.map
