57f5ad2ffec320e1373a6b86aee027c6290c85c3
tannerhawkins <tannerhawkins2703@gmail.com>
2021-12-01 17:06:59 -0500

    Updates

---
+++ static/js/24.a99dd274.chunk.js
@@ -0,0 +1,234 @@
+(this.webpackJsonpAncientPathAdventures=this.webpackJsonpAncientPathAdventures||[]).push([[24],{108:function(e,t,i){"use strict";t.a=i.p+"static/media/homepage-splash.82706964.png"},253:function(e,t,i){"use strict";i.r(t);i(2);var n=i(36),a=i(25),s=i(66),c=i(108),r=i(67),o=i(11),d=i(16),l=i(21),h=i(5);const x=s.a.div`
+  display: flex;
+  flex-flow: column wrap;
+  height: 775px;
+  background-color: ${a.a.COLOR.GREEN};
+  position: relative;
+  display: flex;
+  margin-top: ${a.a.HEADER_HEIGHT};
+`,p=s.a.img`
+  position: absolute;
+  right: 0;
+  height: 100%;
+  width: 60%;
+  max-width: 600px;
+`,j=s.a.h1`
+  color: white;
+  font-weight: 700;
+  font-size: 73px;
+  line-height: 86px;
+  margin-top: 50px;
+  @media (max-width: 800px) {
+    font-size: 60px;
+  }
+`,g=s.a.h3`
+  color: ${a.a.COLOR.GRAY};
+  font-weight: 300;
+  font-size: 37px;
+  line-height: 43px;
+`,b=s.a.div`
+  margin-left: 10%;
+  margin-top: 5%;
+  display: flex;
+  flex-flow: column wrap;
+  max-width: 35%;
+  z-index: 1;
+`,m=s.a.div`
+  display: flex;
+  margin-top: 5%;
+  margin-left: 10%;
+  z-index: 1;
+`,f=Object(s.a)(r.a)`
+  font-size: 30px;
+  line-height: 30px;
+  font-weight: 700;
+  max-width: 200px;
+  height: 60px;
+  padding: 10px 35px;
+`;Object(s.a)(r.a)`
+  background-color: ${a.a.COLOR.DARK_GREEN};
+  margin-left: 19px;
+  font-size: 30px;
+  line-height: 36px;
+  font-weight: 700;
+  max-width: 200px;
+  height: 60px;
+  padding: 10px 35px;
+`;var O=()=>{const e=Object(o.g)(),t=Object(d.c)(l.a),i=Object(d.c)(l.e);return Object(h.jsxs)(x,{children:[Object(h.jsxs)(b,{children:[Object(h.jsx)(j,{children:"Welcome to APA!"}),Object(h.jsx)(g,{children:"Let's get started on your immersive biblical experience!"})]}),Object(h.jsx)(m,{children:Object(h.jsx)(f,{onClick:()=>i?e.push(`${t}/home`):e.push("/signin"),children:"Dashboard"})}),Object(h.jsx)(p,{src:c.a})]})},u=i(90),w=i.p+"static/media/circle.bc41d2fd.png",v=i.p+"static/media/square.94282324.png";const y=s.a.div`
+  position: relative;
+`,E=s.a.div`
+  height: 55vw;
+  width: 100%;
+  @media (max-width: 1000px) {
+    display: flex;
+    flex-flow: column;
+    height: auto;
+  }
+`,R=s.a.div`
+  background: url(${u.a}) no-repeat;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: 100% 100%;
+`,z=s.a.div`
+  display: flex;
+  justify-content: center;
+  flex-flow: row wrap;
+`,k=s.a.div`
+  display: flex;
+  min-width: 500px;
+  margin-left: 30px;
+  align-items: center;
+  justify-content: space-between;
+  @media (max-width: 1000px) {
+    min-width: 0px;
+    justify-content: space-around;
+    margin: 0 auto;
+    width: 90%;
+  }
+`,A=s.a.div`
+  @media (max-width: 1000px) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-top: 30px;
+  }
+`,G=s.a.img`
+  position: absolute;
+  width: 20%;
+  max-width: 350px;
+  @media (max-width: 1000px) {
+    position: relative;
+    width: 40%;
+    margin: 0 auto !important;
+  }
+`,L=s.a.img`
+  width: 60%;
+  max-width: 400px;
+  @media (max-width: 1000px) {
+    width: 40%;
+    min-width: 0px;
+  }
+`,C=s.a.p`
+  font-size: 40px;
+  width: 300px;
+  img + & {
+    margin-left: 50px;
+  }
+  @media (max-width: 600px) {
+    font-size: 4vw;
+    img + & {
+      margin-left: 20px;
+    }
+  }
+`,N=s.a.p`
+  position: absolute;
+  font-weight: 300;
+  font-size: min(2.3vw, 40px);
+  margin: 0 auto;
+  margin-left: 40px;
+  width: 20%;
+  max-width: 300px;
+  font-weight: 500;
+  @media (max-width: 1000px) {
+    position: relative;
+    width: 40%;
+    font-size: 4vw;
+    margin: 0 auto !important;
+  }
+`,$=s.a.h1`
+  color: ${a.a.COLOR.DARK_GREEN};
+  font-size: 90px;
+  padding-left: 12%;
+  padding-top: 300px;
+  @media (max-width: 1000px) {
+    font-size: 50px;
+    padding-top: 30vw;
+    text-align: center;
+    padding-left: 0%;
+  }
+`;var H=()=>Object(h.jsxs)(y,{children:[Object(h.jsxs)(E,{children:[Object(h.jsxs)(A,{children:[Object(h.jsx)(G,{src:w,style:{margin:"5% 0% 0% 5%"}}),Object(h.jsx)(N,{style:{margin:"5% 0% 0% 28%"},children:"Explore creative interactive modules in our Ancient Path Adventure Learning Modules."})]}),Object(h.jsxs)(A,{children:[Object(h.jsx)(G,{src:w,style:{margin:"10% 0% 0% 50%"}}),Object(h.jsx)(N,{style:{margin:"10% 0% 0% 75%"},children:"Each module teaches an aspect of the Bible and combines it with a learning adventure."})]}),Object(h.jsxs)(A,{children:[Object(h.jsx)(N,{style:{margin:"30% 0% 0% 5%"},children:"Featuring the Babies Bible Series. Travel through the Bible with Baby Nahal and friends, in this series of Bible books for babies and children."}),Object(h.jsx)(G,{src:w,style:{margin:"35% 0% 0% 30%"}})]}),Object(h.jsx)(A,{children:Object(h.jsx)(N,{style:{margin:"35% 0% 0% 60%"},children:"Ancient Path Kids, produces biblical stories, animations, coloring books, and more."})})]}),Object(h.jsxs)(R,{children:[Object(h.jsx)($,{children:"Get started today, sign up!"}),Object(h.jsxs)(z,{children:[Object(h.jsxs)(k,{children:[Object(h.jsx)(L,{src:v}),Object(h.jsx)(C,{children:"We give glory, honor and praise to God for giving us the opportunity to spread His Word through creative means. "})]}),Object(h.jsxs)(k,{children:[Object(h.jsx)(L,{src:v}),Object(h.jsx)(C,{children:"We believe that representation matters for our little ones, as the scripture reads:"})]}),Object(h.jsxs)(k,{children:[Object(h.jsx)(C,{children:"\u2018Train up a child in the way he should go, Even when he is old he turns not away from it.\u2019"}),Object(h.jsx)(L,{src:v})]}),Object(h.jsxs)(k,{children:[Object(h.jsx)(C,{children:"\u2013 Mishl\u0115 (Proverbs) 22:6"}),Object(h.jsx)(L,{src:v})]})]})]})]}),P=i(74),_=i(19),T=i(22);const D=Object(s.a)(r.a)`
+  background-color: ${a.a.COLOR.DARK_GREEN};
+  width: 120px;
+  height: 40px;
+  margin-left: 10px;
+`;var I=({className:e})=>{const t=Object(o.g)(),i=Object(d.b)(),n=Object(d.c)(l.e);return Object(h.jsxs)("div",{className:e,"data-test":"login-logout",children:[!n&&Object(h.jsx)(D,{onClick:()=>t.push("/signin"),children:"SIGN-IN"}),n&&Object(h.jsx)(D,{onClick:()=>{_.b.signOut().then((()=>{i(Object(T.a)())}))},children:"LOGOUT"})]})};const B=s.a.header`
+  display: flex;
+  background-color: ${a.a.COLOR.LIGHT_GREEN};
+  justify-content: space-between;
+  align-items: center;
+  height: ${a.a.HEADER_HEIGHT};
+  left: 0;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  z-index: 100000;
+`,K=s.a.div`
+  display: flex;
+  align-items: center;
+  margin-right: 3%;
+`,W=s.a.div`
+  font-family: Playball;
+  color: white;
+  white-space: nowrap;
+  margin-left: 30px;
+  font-size: 30px;
+  font-weight: bold;
+  text-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
+  @media (max-width: 800px) {
+    display: none;
+  }
+`,J=s.a.img`
+  margin-left: 3%;
+  width: 130px;
+`,M=s.a.div`
+  display: flex;
+  align-items: center;
+  margin-left: 3%;
+  :hover {
+    cursor: pointer;
+  }
+`,S=(Object(s.a)(r.a)`
+  margin-right: 10px;
+  box-shadow: none;
+  padding: 10px 20px;
+`,Object(s.a)(I)``);var U=()=>{const e=Object(o.g)();return Object(h.jsxs)(B,{id:"header",children:[Object(h.jsxs)(M,{onClick:()=>e.push("/home"),children:[Object(h.jsx)(J,{src:P.a}),Object(h.jsx)(W,{children:"Ancient Path Adventures"})]}),Object(h.jsx)(K,{children:Object(h.jsx)(S,{})})]})};const q=s.a.div`
+  display: flex;
+  background-color: ${a.a.COLOR.DARK_GREEN};
+  justify-content: center;
+  align-items: center;
+  flex-flow: column;
+  height: 80px;
+  position: relative;
+  width: 100%;
+  z-index: 100000;
+  overflow: hidden;
+`,F=s.a.p`
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 43px;
+  color: white;
+  margin: 0 auto;
+  text-decoration: none;
+`,Y=s.a.p`
+  font-size: 15px;
+  color: ${a.a.COLOR.LIGHT_GREEN};
+  margin: 0 auto;
+`,Q=s.a.a`
+  color: white;
+`;var V=()=>Object(h.jsxs)(q,{id:"footer",children:[Object(h.jsxs)(F,{children:[Object(h.jsx)(Q,{href:"http://ancientpathadventures.com/terms-conditions/",target:"_blank",children:"Terms of Use"}),Object(h.jsx)("span",{children:" - "}),Object(h.jsx)(Q,{href:"http://ancientpathadventures.com/privacy-policy/",target:"_blank",children:"Privacy Policy"})]}),Object(h.jsx)(Y,{children:"Copyright \xa9 2021 Ancient Path Adventures"})]});t.default=()=>Object(h.jsxs)(n.a,{description:"Homepage",children:[Object(h.jsx)(U,{}),Object(h.jsx)(O,{}),Object(h.jsx)(H,{}),Object(h.jsx)(V,{})]})},67:function(e,t,i){"use strict";var n=i(25);const a=i(66).a.div`
+  background-color: ${n.a.COLOR.LIGHT_GREEN};
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  color: white;
+  font-weight: bold;
+  &:hover {
+    filter: brightness(110%);
+    cursor: pointer;
+  }
+`;t.a=a},74:function(e,t,i){"use strict";t.a=i.p+"static/media/white mtn.bb2904a1.png"},90:function(e,t,i){"use strict";t.a=i.p+"static/media/homepage-splash-2.df6b84b2.png"}}]);
+//# sourceMappingURL=24.a99dd274.chunk.js.map
---
+++ static/js/32.21ec24ad.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpAncientPathAdventures=this.webpackJsonpAncientPathAdventures||[]).push([[32],{252:function(e,t,n){"use strict";n.r(t);var c=n(2),a=n(11),h=n(5);const s=Object(c.lazy)((()=>Promise.all([n.e(0),n.e(1),n.e(21)]).then(n.bind(null,232)))),o=Object(c.lazy)((()=>n.e(31).then(n.bind(null,233))));t.default=({match:e})=>Object(h.jsxs)(a.d,{children:[Object(h.jsx)(a.b,{path:`${e.path}/home`,component:s}),Object(h.jsx)(a.b,{path:`${e.path}/create-module`,component:o}),Object(h.jsx)(a.a,{to:`${e.path}/home`})]})}}]);
+//# sourceMappingURL=32.21ec24ad.chunk.js.map
