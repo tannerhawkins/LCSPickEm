0cf1e5b24638ba422ef4981e6e873f5d94b28808
tannerhawkins <tannerhawkins2703@gmail.com>
2022-01-20 19:02:42 -0500

    Deleting Things

--- src/data/epub/EbookExports.js
+++
@@ -1,4 +0,0 @@
-export { default as Book1 } from "./Book1_In_the_Beginning_ebook.epub";
-export { default as Book2 } from "./Book2_Man_and_Womand_ebook.epub";
-export { default as Book3 } from "./Book3_The_First_Sin.epub";
-export { default as Book4 } from "./Book4_Cain_and_Abel_ebook.epub";
